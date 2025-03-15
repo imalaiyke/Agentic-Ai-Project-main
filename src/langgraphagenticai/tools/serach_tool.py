d009142bc5543897864c219eee0872d5599cc7c3
Ike Imala <imalaiyke@gmail.com>
2025-03-15 14:20:30 -0500

    Chatbot with tools

--- src/langgraphagenticai/tools/serach_tool.py
+++ src/langgraphagenticai/tools/serach_tool.py
@@ -1,12 +1,29 @@
 from langchain_community.tools.tavily_search import TavilySearchResults
 #from langchain_community.tools.gmail.utils import GmailToolkit
+from langchain_community.agent_toolkits import SparkSQLToolkit, create_spark_sql_agent
+from langchain_community.utilities.spark_sql import SparkSQL
+from langchain_core.language_models import BaseLanguageModel
+from langchain_core.tools import BaseTool
+from langchain_core.tools.base import BaseToolkit
+from pydantic import ConfigDict, Field
+from langchain_community.tools.spark_sql.tool import (
+    InfoSparkSQLTool,
+    ListSparkSQLTool,
+    QueryCheckerTool,
+    QuerySparkSQLTool,
+)
 from langgraph.prebuilt import ToolNode
 
 def get_tools():
     """
     Return the list of tools to be used in the chatbot
     """
-    tools=[TavilySearchResults(max_results=2)]
+    tools=[TavilySearchResults(max_results=2),
+           QuerySparkSQLTool(db=self.db),
+            InfoSparkSQLTool(db=self.db),
+            ListSparkSQLTool(db=self.db),
+            QueryCheckerTool(db=self.db),
+            ]
     return tools
 
 def create_tool_node(tools):
